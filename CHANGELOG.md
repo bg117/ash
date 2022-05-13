76544b45cc68450468d70147224c78e4e4a95a9e
bg117 <kiangabrielarambulo@gmail.com>
2022-05-13 10:58:29 +0800

    add entry to changelog; delete publish files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # ASH Changelog
 
-## Latest: version 1.0.0
+## Latest: version 1.1.0
+
+Minor changes.
+
+- Improved `help` command
+- Added `ls` and `cd`
+- Fixed spelling and grammar in `help` command
+
+## version 1.0.0
 
 First release of ASH (Application shell).
--- publish.sh
+++
@@ -1,6 +0,0 @@
-dotnet publish src/ash.csproj -p:PublishProfile=src/Properties/Winx64.pubxml
-dotnet publish src/ash.csproj -p:PublishProfile=src/Properties/Winx86.pubxml
-dotnet publish src/ash.csproj -p:PublishProfile=src/Properties/Linuxx64.pubxml
-dotnet publish src/ash.csproj -p:PublishProfile=src/Properties/Winx64-rel.pubxml
-dotnet publish src/ash.csproj -p:PublishProfile=src/Properties/Winx86-rel.pubxml
-dotnet publish src/ash.csproj -p:PublishProfile=src/Properties/Linuxx64-rel.pubxml
